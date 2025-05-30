7bab03a2b9a16d560740bebdd76d0a3de2ed774f
Logan Miller <loganmiller040402@gmail.com>
2025-05-29 19:55:17 -0700

    Basic library setup

---
+++ c-logger/c-logger.c
@@ -0,0 +1,12 @@
+#include "c-logger.h"
+
+#include <stdio.h>
+
+void test_logger(void) {
+    // This function is a placeholder for testing the logger functionality.
+    // You can implement your logging logic here.
+    // For example, you might want to log messages to a file or console.
+    
+    // Example log message
+    printf("Logger test function called.\n");
+}
---
+++ c-logger/c-logger.h
@@ -0,0 +1,6 @@
+#ifndef C_LOGGER_H
+#define C_LOGGER_H
+
+void test_logger(void);
+
+#endif // C_LOGGER_H
